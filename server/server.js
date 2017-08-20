a2131f0f72cacc87d75c043ad36d152e71051297
Erin Hinson <erinh@xceleration.com>
2017-08-19 20:24:45 -0400

    Merge

--- server/server.js
+++ server/server.js
@@ -7,6 +7,18 @@ import falcorExpress from 'falcor-express';
 import falcorRouter from 'falcor-router';
 import routes from './routes.js';
 import path from 'path';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { renderToStaticMarkup } from 'react-dom/server';
+import ReactRouter from 'react-router';
+import { StaticRouter } from 'react-router';
+import * as hist from 'history';
+import rootReducer from '../src/reducers';
+import reactRoutes from '../src/routes';
+import fetchServerSide from './fetchServerSide';
+import Root from '../src/containers/Root';
+import { ConnectedRouter, push } from 'react-router-redux';
 
 const app = express();
 app.server = http.createServer(app);
@@ -30,9 +42,7 @@ let cache = {
   ]
 };
 
-const model = new falcor.Model({
-  cache: cache
-});
+const model = new falcor.Model({ cache: cache });
 
 app.use(
   '/model.json',
@@ -43,20 +53,82 @@ app.use(
 
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-  Article.find((err, docs) => {
-    const myArticles = docs
-      .map(item => {
-        return `<h2>${item.articleTitle}</h2>
-      ${item.articleContent}`;
-      })
-      .join('<br/>');
-    res.send(`<h1>Publishing App Initial Application!</h1>
-      ${myArticles}`);
-  });
-});
+let handleServerSideRender = (req, res) => {
+  let initMOCKstore = fetchServerSide();
+  let context = {};
+  const store = createStore(rootReducer, initMOCKstore);
+  const memoryHistory = hist.createMemoryHistory();
+  // Render the component to a string
+  const html = renderToStaticMarkup(
+    <Provider store={store}>
+      <StaticRouter
+        location={req.url}
+        context={context}
+        history={memoryHistory}
+      >
+        <Root />
+      </StaticRouter>
+    </Provider>
+  );
+
+  // Grab the initial state from our Redux store
+  const preloadedState = store.getState();
+  if (context.url) {
+    res.writeHead(301, {
+      Location: context.url
+    });
+    res.end();
+  } else {
+    // res.write(`
+    //   <!doctype html>
+    //   <div id="root">${html}</div>
+    // `);
+    // res.end();
+    res.send(renderFullHtml(html, preloadedState));
+  }
+
+  // Send the rendered page back to the client
+};
+
+let renderFullHtml = (html, initialState) => {
+  return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>Bloggerin Server Side Rendering</title>
+    <link href="https://fonts.googleapis.com/css?family=Zilla+Slab:300,400" rel="stylesheet">
+    <link href="https://cdn.quilljs.com/1.3.0/quill.snow.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+    <h1>Server side publishing app</h1>
+    <div id="root">${html}</div>
+    <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState).replace(
+      /</g,
+      '\\u003c'
+    )}</script>
+    <script src="app.js"></script>
+    </body>
+    </html>
+`;
+};
+app.use(handleServerSideRender);
+
+// app.get('/', (req, res) => {
+//   Article.find((err, docs) => {
+//     const myArticles = docs
+//       .map(item => {
+//         return `<h2>${item.articleTitle}</h2>
+//       ${item.articleContent}`;
+//       })
+//       .join('<br/>');
+//     res.send(`<h1>Publishing App Initial Application!</h1>
+//       ${myArticles}`);
+//   });
+// });
+//
 // app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
+//   res.sendFile(path.join(__dirname, '.dist/index.html'));
 // });
 
 app.server.listen(process.env.PORT || 3001);
